8cf31e34c42cad3e1acb4f8d3663d55d65a83d71
Jonathan Koser <jonathan.koser@gmail.com>
2019-01-14 22:24:12 -0600

    Couple new things
    Changed the colors, fixed a bit of logic.

--- Map/js/main.js
+++ Map/js/main.js
@@ -1,32 +1,32 @@
 var mapboxAccessToken = 'pk.eyJ1Ijoiamtvc2VyIiwiYSI6ImNpbWtxNTg2NzAxNGl2cGtnY3k1dGU3aXEifQ.VbZ8l9bL6wsh8dgaI8gTuw';
-var map = L.map('map').setView([43.073051, 	-89.401230], 13);
+var map = L.map('map').setView([43.073051, 	-89.401230], 12);
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=' + mapboxAccessToken, {
-    id: 'mapbox.light',
+    id: 'mapbox.dark',
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
         '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
         'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
 }).addTo(map);
 
 function getColor(numPts) {
-    return numPts > 9 ? '#662506' :
-       numPts > 8  ? '#993404' :
-       numPts > 7  ? '#cc4c02' :
-       numPts > 6  ? '#ec7014' :
-       numPts > 5  ? '#fe9929' :
-       numPts > 4  ? '#fec44f' :
-       numPts > 3  ? '#fee391' :
-       numPts > 2  ? '#fff7bc' :
-       numPts > 1  ? '#ffffe5' :
-                     'None' ;
+    return numPts >= 9 ? ['#7f0000', 'white'] :
+       numPts >= 8  ? ['#b30000', 'white'] :
+       numPts >= 7  ? ['#d7301f', 'white'] :
+       numPts >= 6  ? ['#ef6548', 'white'] :
+       numPts >= 5  ? ['#fc8d59', 'white'] :
+       numPts >= 4  ? ['#fdbb84', 'white'] :
+       numPts >= 3  ? ['#fdd49e', 'white'] :
+       numPts >= 2  ? ['#fee8c8', 'white'] :
+       numPts >= 1  ? ['#fff7ec', 'white'] :
+                     ['None', 'None'] ;
 }
 
 function style(feature) {
     return {
-        fillColor: getColor(feature.properties.NUMPOINTS),
-        weight: 2,
-        opacity: 1,
-        color: 'white',
+        fillColor: getColor(feature.properties.NUMPOINTS)[0],
+        weight: 1,
+        opacity: 0.7,
+        color: getColor(feature.properties.NUMPOINTS)[1],
         dashArray: '3',
         fillOpacity: 0.7
     };
